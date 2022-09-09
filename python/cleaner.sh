72e06654bb96764c73a7bda854cb286feda91ab3
gavinbarnard <gavin.r.barnard@gmail.com>
2022-09-09 01:35:55 +0000

    check for new blocks during cleaning

--- python/cleaner.sh
+++ python/cleaner.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
-cd /home/monero/p2pooler/python
+
+CONFIG_FILE=~/.config/p2pooler-py.json
+
+jq . $CONFIG_FILE
+
+if [ $? != 0 ];
+then
+	echo bad config
+	exit;
+fi
+
+site=`jq -r .site_ip $CONFIG_FILE`
+
+BASEPATH=/home/monero/p2pooler/python
+REWARDLOG=$BASEPATH/reward.log
+CLEANERLOG=$BASEPATH/cleaner.log
+
+cd $BASEPATH
 source ../venv/bin/activate
-echo -- start -- >> /home/monero/p2pooler/python/cleaner.log
-echo $(date --iso-8601=s) >> /home/monero/p2pooler/python/cleaner.log
-python cleaner.py >> /home/monero/p2pooler/python/cleaner.log
-echo $(date --iso-8601=s) >> /home/monero/p2pooler/python/cleaner.log
-echo -- end -- >> /home/monero/p2pooler/python/cleaner.log
+echo -- start -- >> $CLEANERLOG 
+echo $(date --iso-8601=s) >> $CLEANERLOG 
+python cleaner.py >> $CLEANERLOG 
+echo $(date --iso-8601=s) >> $CLEANERLOG 
+echo -- end -- >> $CLEANERLOG 
+
+echo -- start -- >> $REWARDLOG
+echo $(date --iso-8601=s) >> $REWARDLOG 
+count=$(curl -s http://$site/1/needreward | jq '.|length')
+if [[ "$count" != "0" ]]; then
+    echo "$count blocks found that need to be split" >> $REWARDLOG
+    #PAYOUT IS DISABLED BY DEFAULT UNCOMMENT THE NEXT LINE TO ENABLE
+    #python reward_splitter.py >> $REWARDLOG  # uncomment to reward splits for payer.py to payout from.
+else
+    echo "no blocks found that need to be split" >> $REWARDLOG
+fi
+echo $(date --iso-8601=s) >> $REWARDLOG 
+echo -- end -- >> $REWARDLOG
