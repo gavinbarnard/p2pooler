ab1b89fe8fa6e863692767eeac6f45636b1a115c
XMRig <support@xmrig.com>
2023-02-02 11:14:29 +0700

    v6.19.0-dev

--- src/version.h
+++ src/version.h
@@ -1,7 +1,12 @@
 /* XMRig
+<<<<<<< HEAD
  * Copyright (c) 2018-2022 SChernykh   <https://github.com/SChernykh>
  * Copyright (c) 2016-2022 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  * Copyright (c) 2022      grb         <https://github.com/gavinbarnard>
+=======
+ * Copyright (c) 2018-2023 SChernykh   <https://github.com/SChernykh>
+ * Copyright (c) 2016-2023 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+>>>>>>> 8b937da (v6.19.0-dev)
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -34,7 +39,9 @@
 #define APP_VER_PATCH  0
 
 #ifdef _MSC_VER
-#   if (_MSC_VER >= 1920)
+#   if (_MSC_VER >= 1930)
+#       define MSVC_VERSION 2022
+#   elif (_MSC_VER >= 1920 && _MSC_VER < 1930)
 #       define MSVC_VERSION 2019
 #   elif (_MSC_VER >= 1910 && _MSC_VER < 1920)
 #       define MSVC_VERSION 2017
