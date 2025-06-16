6e87602a92e5bcf93793b3f39ae1832553033e4e
XMRig <support@xmrig.com>
2025-06-16 21:28:14 +0700

    v6.23.1-dev

--- src/version.h
+++ src/version.h
@@ -23,15 +23,15 @@
 #define APP_ID        "xmrig-proxy"
 #define APP_NAME      "xmrig-proxy"
 #define APP_DESC      "XMRig Stratum proxy"
-#define APP_VERSION   "6.23.0"
+#define APP_VERSION   "6.23.1-dev"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
-#define APP_COPYRIGHT "Copyright (C) 2016-2024 xmrig.com"
+#define APP_COPYRIGHT "Copyright (C) 2016-2025 xmrig.com"
 #define APP_KIND      "proxy"
 
 #define APP_VER_MAJOR  6
 #define APP_VER_MINOR  23
-#define APP_VER_PATCH  0
+#define APP_VER_PATCH  1
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1930)
